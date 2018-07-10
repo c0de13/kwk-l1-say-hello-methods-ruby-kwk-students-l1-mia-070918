6508cdd3fded6dbcc3b19a41ae46931100421b1b
c0de13 <meganmpuppy@gmail.com>
2018-07-10 14:38:40 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,8 @@
 # Build your say_hello method here
 
+#say_hello is the defult, so if nothing is passed, Hello Ruby Programmer! is printed instead
+
+
 def say_hello(name = "Ruby Programmer")
   puts "Hello #{name}!"
   
